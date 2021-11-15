8171481b348202e479c736e442cb439a381cabfb
Kingsley goodluck <goodluckcanhelp@gmail.com>
2021-11-15 19:23:49 +0100

    fixed nav

--- src/components/CarouselItem.js
+++ src/components/CarouselItem.js
@@ -3,43 +3,31 @@ import React from 'react'
 
 import AwesomeSlider from 'react-awesome-slider';
 
+import 'react-awesome-slider/dist/styles.css';
 import banner1 from '../res/images/banner.jpg'
 import banner2 from '../res/images/banner2.jpg'
-// Import css files
-
-import Slider from "react-slick";
-
 const CarouselItem  = () =>{
-
-    const settings ={
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
-      };
-
     return (
-       <Slider {...settings}>
-      <div>
-        <h3>1</h3>
-      </div>
-      <div>
-        <h3>2</h3>
-      </div>
-      <div>
-        <h3>3</h3>
-      </div>
-      <div>
-        <h3>4</h3>
-      </div>
-      <div>
-        <h3>5</h3>
-      </div>
-      <div>
-        <h3>6</h3>
-      </div>
-    </Slider>
+        <AwesomeSlider 
+            
+        organicArrows={true} style={{height:500}}>
+        
+        <div>
+            <div>
+            <h3> house at lekki</h3>
+
+            <img src="https://images.unsplash.com/photo-1599809275671-b5942cabc7a2?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmVhbCUyMGVzdGF0ZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80" />
+
+            </div>
+        </div>
+        <div>
+            <img src={banner2} />
+        </div>
+        <div>
+            <img src={banner2} />  
+        </div>
+        
+      </AwesomeSlider>
     )
 }
 
